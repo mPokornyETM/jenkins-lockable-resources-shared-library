4c657fc502e1529f54e8a873001d332f4ff73c70
Pokorny Martin <martin.pokorny@etm.at>
2023-02-05 15:50:31 +0100

    f

--- src/io/jenkins/library/lockableresources/Utils.groovy
+++ src/io/jenkins/library/lockableresources/Utils.groovy
@@ -1,7 +1,7 @@
-
+#!groovy
+package io.jenkins.library.lockableresources;
 
 class Utils {
-    
   //---------------------------------------------------------------------------
   public static void fixNullMap(Map map) {
     if (map == null) {
